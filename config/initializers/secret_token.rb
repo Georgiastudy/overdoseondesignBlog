bd00d07b23fcc35ed2bdd833a98851872752d9d1
Rodger <rodger@rodger-Lenovo.(none)>
2013-01-18 03:20:34 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overdose::Application.config.secret_token = 'a26fbb5abbaafe882898925f4e3d59086dbcbcad273bbdc0d0ca587738bb7a2b1a0a9bbe045ff1ba7577f6a0034bab681462d745cac6bd7c49388e07ee35d17f'
